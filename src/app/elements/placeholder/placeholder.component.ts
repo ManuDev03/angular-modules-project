b7577eb0e5c7cfd7bc866e402676f95173f74203
ManuDev03 <=manuprajan03@gmail.com>
2021-11-08 17:26:54 +0530

    creating structural directives for placeholder components

--- src/app/elements/placeholder/placeholder.component.ts
+++ src/app/elements/placeholder/placeholder.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class PlaceholderComponent implements OnInit {
   @Input() header = true;
-  @Input() lines = 3 
+  @Input() lines = 0
 
   constructor() { }
 
