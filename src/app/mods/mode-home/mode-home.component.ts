479a841a951436700b64692cf2243d7337942b67
ManuDev03 <=manuprajan03@gmail.com>
2021-11-10 18:39:16 +0530

    listing accordion elements

--- src/app/mods/mode-home/mode-home.component.ts
+++ src/app/mods/mode-home/mode-home.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModeHomeComponent implements OnInit {
   modelOpen =false
+
+  items = [ 
+  { title:'which is the best country to visit', content:'India is the best country ever'},
+  { title:'which are the best places in India ', content:'Best places in india are in Rajastan,Kerala,Arnunachal pradesh'},
+  { title:'which are the best places in Kerala', content:'Best places in kerala are in Kochi,thalassery,kanyakumari'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
