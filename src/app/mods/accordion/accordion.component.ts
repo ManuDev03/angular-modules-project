f0d7f70875daf1ed00c7833a390f164af909bb56
ManuDev03 <=manuprajan03@gmail.com>
2021-11-10 19:05:51 +0530

    accordion toggle functionality

--- src/app/mods/accordion/accordion.component.ts
+++ src/app/mods/accordion/accordion.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
   @Input() items:any = []
+  opendItemIndex = 0
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onClick(index:number){
+
+    if(index === this.opendItemIndex)
+    {
+      this.opendItemIndex = -1
+    }
+    else 
+    {
+    this.opendItemIndex = index
+    }
   }
 
 }
